36ab6bbdd809e706ab7d16860647cb82ced00d23
keiakihito <kkatsumi.git@gmail.com>
2024-08-19 17:16:28 -0700

    Implement createItem

--- next-market/pages/api/item/create.js
+++ next-market/pages/api/item/create.js
@@ -1,6 +1,25 @@
-const createItem = (req, res) => {
-    console.log(req.body.title)
-    return res.status(200).json({message:"Create a new item"})
+import connectDB from "../../../utils/database"
+import {ItemModel} from "../../../utils/schemaModels"
+
+const createItem = async(req, res) => {
+    try{
+        //Connect to DB
+        await connectDB();
+
+        //ItemModel.create() writes data to the database.
+        //Update DB schema. if it fails, it goes to catch scope.
+        await ItemModel.create(req.body);
+        console.log("Success: Item created in MongoDB");
+
+        return res.status(200).json({message:"Create a new item."});
+    }catch(err){
+        console.error("Fail: Item was not created in MongoDB", err);
+        return res.status(400).json({message:"Failed to create a new item."});
+    }
+
+
 }
 
 export default createItem
+
+
