e0b17100e0d46d423bcb4a5b7a7e7481e12f6146
keiakihito <kkatsumi.git@gmail.com>
2024-08-20 18:59:57 -0700

    Changing ts to tsx schemaModels.ts

---
+++ next-market/utils/schemaModels.ts
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+interface ItemDataType{
+    title: string
+    image: string
+    price: string
+    description: string
+    email: string
+}
+
+interface UserDataType{
+    name: string
+    email: string
+    password: string
+}
+
+const ItemSchema = new Schema<ItemDataType>({
+    title: String,
+    image:String,
+    price: String,
+    description: String,
+    email: String,
+})
+
+const UserSchema = new Schema<UserDataType>({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+})
+
+
+
+export const ItemModel = mongoose.models.Item || mongoose.model<ItemDataType>("Item", ItemSchema);
+export const UserModel = mongoose.models.User || mongoose.model<UserDataType>("User", UserSchema);
