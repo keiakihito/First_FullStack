f58e7ae6066ed79188b89c116d78deb86fd1ac44
keiakihito <kkatsumi.git@gmail.com>
2024-08-22 17:04:03 -0700

    Implement register.js and need to implement login.js

--- next-market/pages/api/user/register.ts
+++ next-market/pages/api/user/register.ts
@@ -5,10 +5,17 @@ import {ExtendedNextApiRequestUser, ResMessageType} from "../../../utils/types";
 
 
 const registerUser = async (req: ExtendedNextApiRequestUser, res: NextApiResponse<ResMessageType>) => {
+let debug: boolean = false;
 
     try{
         await connectDB();
-        await UserModel.create(req.body);
+        if(debug){
+            console.log("Request Body to DB:", req.body);
+        }
+        const newUser = await UserModel.create(req.body);
+        if(debug){
+            console.log("User Create to DB:", newUser);
+        }
         return res.status(200).json({message: "Success to create a new user registration "});
     }catch(err){
         return res.status(400).json({message:"Failed to create a new user"});
---
+++ next-market/pages/user/register.js
@@ -0,0 +1,59 @@
+import {useState} from "react"
+
+const Register = () =>{
+
+    //React pass data through state
+    //This is a place to save name from user input
+    //"" can hold a placeholder such as "Keita"
+    //setName can modify value in the name as a setter
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async(e) =>{
+        e.preventDefault(); // It prevents reload when user input data.
+        try {
+            //Sending user data from input to api/user/register.js
+            //It requests backend to create a new user, which means method is POST.
+            //Need to check the sending data to backend is success or not.
+            const response = await fetch("http://localhost:3000/api/user/register", {
+                method: "POST",
+                headers: {
+                    //Sending data with json format
+                    "Accept": "application/json",
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    //JSON.stringfy convert user data with JSON when it sends to backend
+                    //Store user input here
+                    name: name,
+                    email: email,
+                    password: password
+                })
+            })
+            //Need to check sending data to backend is success or not.
+            //Response from backend should be in the jsonData
+            const jsonData = await response.json();
+            alert(jsonData.message);
+        }catch(err){
+            alert("Fail to user registration");
+        }
+    }
+
+    return(
+        <div>
+            <h1>User Registration</h1>
+            <form onSubmit={handleSubmit}>
+                {/*When the user input the name, e holds the value */}
+                {/*e is passed to setName to store value to name. e.target.value specifies name in the e*/}
+                {/*Then name from user input is store to value by setName*/}
+                <input value = {name} onChange = {(e) => setName(e.target.value)} type="text" name="name" placeholder="Name" required/>
+                <input value = {email} onChange = {(e) => setEmail(e.target.value)} type="text" name="email" placeholder="Email Address" required/>
+                <input value = {password} onChange = {(e) => setPassword(e.target.value)} type="text" name="password" placeholder="Password" required/>
+                <button>Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default Register;
